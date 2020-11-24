ec92697e8a67d1d80353ed0a0916f08b0c02ca52
David Dooley <ddooley@Davids-MacBook-Pro.local>
2020-11-24 11:17:56 -0500

    Added index builder function

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ class MyMainWindow(QtWidgets.QMainWindow):
         uic.loadUi(os.getcwd() + '/MyMainWindow.ui', self)
 
         ###Initialize any necessary variables here
-        self.dbpath = ""
-        self.crRNA_dict = {}
-        self.switcher = [1,1,1,1,1,1,1]  # for keeping track of where we are in the sorting clicking for each column
-        self.path1 = ""
-        self.path2 = ""
-        self.path3 = ""
+        self.crRNA_dict = {} #Stores crRNA info, key = sequence
+        self.switcher = [1,1,1,1,1,1,1]  #Keeps track of where we are in the sorting clicking for each column
+        self.casper_info = os.getcwd() + "/bin/CASPERinfo" #Supplementary file for Off-Target and Index Builder algorithms
+        self.path1 = "" #Path to crRNA file
+        self.path2 = "" #Path to ref file
+        self.path3 = "" #Path to query file
         self.query_dict = {}
 
         ### crRNA table settings
@@ -26,12 +26,7 @@ class MyMainWindow(QtWidgets.QMainWindow):
         self.crRNA_table.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
         self.crRNA_table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
         self.crRNA_table.setSelectionMode(QtWidgets.QAbstractItemView.MultiSelection)
-#        self.back_button.clicked.connect(self.goBack)
         self.crRNA_table.horizontalHeader().sectionClicked.connect(self.table_sorting)
-#        self.actionSave.triggered.connect(self.save_data)
-#        self.actionOpen.triggered.connect(self.open_data)
-#        self.actionOff_Target_Analysis.triggered.connect(self.Off_Target_Analysis)
-#        self.actionCoTargeting.triggered.connect(self.open_cotarget)
 
         ###Make sure window is centered upon start-up
         self.mwfg = self.frameGeometry()  ##Create frame geometry
@@ -43,7 +38,7 @@ class MyMainWindow(QtWidgets.QMainWindow):
         self.browse_1.clicked.connect(self.get_file1)
         self.browse_2.clicked.connect(self.get_file2)
         self.browse_3.clicked.connect(self.get_file3)
-        self.run_OT.clicked.connect(self.run_mash)
+        self.run_OT.clicked.connect(self.run_all)
         self.show()
 
     """ Start defining functions here """
@@ -64,7 +59,7 @@ class MyMainWindow(QtWidgets.QMainWindow):
         else:
             QtWidgets.QMessageBox.question(self, "Error!","You must select a .csv file.\n\nTry again!",QtWidgets.QMessageBox.Ok)
 
-        
+
 
 
     def get_file2(self):
@@ -121,12 +116,27 @@ class MyMainWindow(QtWidgets.QMainWindow):
             self.crRNA_table.setItem(index, 6, strand)
 
         self.crRNA_table.resizeColumnsToContents()
-    
+
+    def run_all(self):
+        """
+        This is the master command that runs when GUI button is clicked for running the entire analysis.
+        It calls the functions that run Mash, Index Builder, Off-Target Algorithm, and requisite parsing
+        functions.
+        """
+        self.run_mash()
+        ###Filter bad sequences out here!###
+        self.build_index()
+
     def run_mash(self):
+        """
+        This function rans Mash and saves the output to self.mash_out_path
+        """
+
         if (self.path2 != "" and self.path3 != ""):
-            ###Set up command
+            ###Set up command###
             # <exe path> dist <options> <ref seq> <query seq>
             # Sketch size by default 10000, kmer = 16
+            ####################
 
             ###Initialize variables
             ref_file = self.path2
@@ -139,7 +149,25 @@ class MyMainWindow(QtWidgets.QMainWindow):
             os.system(cmd) ###Run the command
         else:
             QtWidgets.QMessageBox.question(self, "Error!","You must select reference and query .fasta files.",QtWidgets.QMessageBox.Ok)
-###Index builder function
+
+    def build_index(self, input_fasta):
+    ###"path to exe" "endo" "PAM" "org_code" "FALSE" "output dir" "CASPERinfo" "query file" "org_name "guide length" "seed length" " "
+        if self.mash_out_path: ###Makes sure that Mash has been run first
+            path_to_exe = os.getcwd() + "/bin/index_builder_mac"
+            org_code = "temp"
+            endo = "spCas9"
+            pam = "NGG"
+            output_dir = os.getcwd()
+            self.index = output_dir + "temp_spCas9.cspr"
+            org_name = "temp_index"
+            guide_len = "20"
+            seed_len = "16"
+            index_cmd = '"' + path_to_exe + '" "' + endo + '" "' + pam + '" "' + org_code + '" "' + "FALSE" + '" "' + output_dir + '" "' + self.casper_info + '" "' + input_fasta + '" "' + org_name + '" "' + guide_len + '" "'  + seed_len + '" " "'
+            print(index_cmd)
+            os.system(index_cmd)
+
+        else:
+            QtWidgets.QMessageBox.question(self, "Error!","Run Mash first!",QtWidgets.QMessageBox.Ok)
 
 ###Off-target function
 
